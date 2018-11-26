9eb8ec60355198564219bc2c85d365184d477385
Robert Klim <kontakt.robert.klim@gmail.com>
2018-11-26 20:20:15 +0100

    Aply pixels color change using algorithm: https://web.archive.org/web/20160418004149/http://freespace.virgin.net/hugo.elias/graphics/x_water.htm

--- js/main.js
+++ js/main.js
@@ -1,7 +1,63 @@
+let cols;
+let rows;
+
+let currentState;
+let previousState;
+let dampening = 0.99;
+
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(400, 400);
+
+    cols = width;
+    rows = height;
+
+    currentState = new Array(cols);
+    previousState = new Array(cols);
+
+    // Make 2D array
+    for (let i = 0; i < cols; i++) {
+        currentState[i] = new Array(rows);
+        previousState[i] = new Array(rows);
+    }
+
+    // Insert values
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            currentState[i][j] = 100;
+            previousState[i][j] = 100;
+        }
+    }
+    // console.table(currentState);
+
 }
 
 function draw() {
     background(0);
+
+    loadPixels();
+    for (let i = 1; i < cols - 1; i++) {
+        for (let j = 1; j < rows - 1; j++) {
+            currentState[i][j] = (
+                (previousState[i - 1][j] + 
+                previousState[i + 1][j] + 
+                previousState[i][j - 1] + 
+                previousState[i][j + 1]) / 2) - 
+                currentState[i][j];
+            currentState[i][j] = currentState[i][j] * dampening;
+            // let col = color(255, 102, 204);
+            let col = color(currentState[i][j]);
+            let index = (i + j * cols) * 4;
+            pixels[index] = red(col);
+            pixels[index + 1] = green(col);
+            pixels[index + 2] = blue(col);
+            pixels[index + 3] = alpha(col);
+       }
+    }
+    updatePixels();
+
+    let tempState = previousState;
+    previousState = currentState;
+    currentState = tempState;
+    
+
 }
